32691ff508a850d82db600fec128e22278088b9b
lenkton <lenktongm@gmail.com>
2025-08-29 23:17:15 +0300

    feat: broadcast newly created rooms to everyone

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,7 +4,7 @@ class Room < ApplicationRecord
   has_many :participations, dependent: :destroy
   has_many :users, through: :participations
 
-  # after_commit { broadcast_prepend_to :rooms }
+  after_create_commit { broadcast_append_to :rooms }
   # after_commit { broadcast_replace_to "room_#{id}" }
 
   validates :name, uniqueness: true
