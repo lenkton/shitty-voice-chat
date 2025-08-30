737e9a05abca88fa9cbc35f01d7d5d4a271995ac
lenkton <lenktongm@gmail.com>
2025-08-30 14:45:58 +0300

    refactor: move hotwire handling from Participation model into RoomJoiner service

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -4,16 +4,11 @@ class Participation < ApplicationRecord
   belongs_to :user
   belongs_to :room
 
-  after_create_commit :update_room_users_list, :after_create_update_user_button
   after_destroy_commit :remove_user_from_room_list, :after_destroy_update_user_button
 
   validates :user_id, uniqueness: { message: 'can join only one room at a time' }
 
   private
-
-  def update_room_users_list
-    broadcast_append_to([room, :users], partial: 'rooms/room_user', locals: { user: user }, target: :room_users)
-  end
 
   def remove_user_from_room_list
     broadcast_remove_to([room, :users], target: "room_user_#{user.id}")
--- app/services/room_joiner.rb
+++ app/services/room_joiner.rb
@@ -15,9 +15,32 @@ class RoomJoiner
 
     @room.users << @user
 
+    update_room_users_list
+    update_join_leave_button
+
     Success.new({ room: @room })
   rescue ActiveRecord::RecordInvalid => e
     errors = e.record.errors.full_messages
     Failure.new(errors: errors)
+  end
+
+  private
+
+  def update_room_users_list
+    Turbo::StreamsChannel.broadcast_append_to(
+      [@room, :users],
+      partial: 'rooms/room_user',
+      locals: { user: @user },
+      target: :room_users
+    )
+  end
+
+  def update_join_leave_button
+    Turbo::StreamsChannel.broadcast_replace_to(
+      [@user, @room],
+      partial: 'rooms/join_leave_button',
+      locals: { user: @user, room: @room },
+      target: :join_leave_button
+    )
   end
 end
