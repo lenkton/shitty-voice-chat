782a74b3589870bbc374f1d855bf662c7494fe4a
lenkton <lenktongm@gmail.com>
2025-08-30 16:43:45 +0300

    feat: clear the message input field after sending

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,10 +3,26 @@
 class MessagesController < ApplicationController
   def create
     message = Message.build(**message_params, user: current_user)
-    if message.save
-      head :no_content
-    else
-      render json: message.errors.full_messages, status: :unprocessable_content
+    respond_to do |format|
+      if message.save
+        format.json { head :no_content }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(
+            'chat_form',
+            partial: 'rooms/chat_form',
+            locals: { room: message.room }
+          )
+        end
+      else
+        format.json { render json: message.errors.full_messages, status: :unprocessable_content }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(
+            'alerts',
+            partial: 'alerts',
+            locals: { messages: @room.errors.full_messages }
+          )
+        end
+      end
     end
   end
 
